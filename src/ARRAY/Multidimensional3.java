ee4bd786d831d444f5bc0cf66a78fdc3428c59eb
DianaSDET <udiana999@gmail.com>
2021-09-18 12:13:50 -0500

    animal update

---
+++ src/ARRAY/Multidimensional3.java
@@ -0,0 +1,19 @@
+package ARRAY;
+
+import java.util.Arrays;
+
+public class Multidimensional3 {
+
+    public static void main(String[] args) {
+
+        int[] ids = {3, 4, 2, 34, 56, 7};
+
+        int[][] multiDem = {{60123, 60124, 60123}, {9123, 8124, 9125}, {5123, 5214, 5215}};
+
+        //multiDem [2][4]=5234;
+
+        System.out.println(Arrays.deepToString(multiDem));
+
+
+    }
+}
