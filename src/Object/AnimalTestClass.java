1d435a0f3d7d7a35c6eb1d3ae273c5f1c26fdafc
DianaSDET <udiana999@gmail.com>
2021-09-18 12:10:35 -0500

    animal update

---
+++ src/Object/AnimalTestClass.java
@@ -0,0 +1,12 @@
+package Object;
+
+public class AnimalTestClass {
+    public static void main(String[] args) {
+
+        Animal cat = new Animal();
+
+//        cat.name = "Tom";
+        System.out.println(cat.name);
+    }
+
+}
