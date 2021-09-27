392c6c44220ff88a8bbb5122afb726bbfdb45849
DianaSDET <udiana999@gmail.com>
2021-09-27 18:32:47 -0500

    Age Calculator

--- src/Methods/AgeTest.java
+++ src/Methods/AgeTest.java
@@ -1,16 +1,21 @@
 package Methods;
 
 public class AgeTest {
+
     public static void main(String[] args) {
+
         ageCalculator tom = new ageCalculator();
-        tom.ageCalculator(1984);
 
- tom.ageCalculator(2018, "John");
+        tom.ageCalculator(1990);
 
+        int b = tom.ageCalculator();
+
+        if (b == 25) {
+            System.out.println("you must be happy");
+        }
+
+        String x = tom.ageCalculator(2018, "John");
+        //  System.out.println(x);
     }
-
-
 }
-
-
 
--- src/Methods/ageCalculator.java
+++ src/Methods/ageCalculator.java
@@ -2,31 +2,46 @@ package Methods;
 
 public class ageCalculator {
 
-    // create a method that will take one integer parameter for your bith year
-    // this method simply calculates and prints your age
+    // create a method that will take one int parameter for your birth year
+    // this method simply calculates and prints you age
 
-    public void ageCalculator (int birthyear){
-        int age =2021-birthyear;
-        System.out.println("Your age is: "+age);
 
+    public void ageCalculator(int birthYear){
+
+        int age = 2021 - birthYear;
+        System.out.println("Your age is: "+ age);
 
     }
+    public int ageCalculator (){
+
+
+        int age = 2021 - 2015;
+        System.out.println("Your age is: "+ age);
+
+        return age;
+    }
+    // overload very first method by passing a string parameter
+    // which is going to be name of the person
+    // and print out the age of the person as:
+    // ".... your age is  ...."
+
+    public String ageCalculator(int birthYear, String name){
+
+        int age = 2021 - birthYear;
+        System.out.println(name+" your age is: "+ age);
+
+        return "*";
+    }
+
+
 
     public static void main(String[] args) {
-        ageCalculator jerry = new ageCalculator();
-        jerry.ageCalculator(2013);
+
+        ageCalculator tom = new ageCalculator();
+
+        tom.ageCalculator(2013);
+
     }
-// overloading this method by passing a string parameter
-    // which is going to be name of the person
-    // and print out age of the person as
-    // " .... your age is ....."
 
-    public String ageCalculator (int birthyear, String name){
-
-        int age =2021-birthyear;
-        System.out.println(name+ " Your age is: "+age);
-
-        return ""+age;
-    }
 
 }
